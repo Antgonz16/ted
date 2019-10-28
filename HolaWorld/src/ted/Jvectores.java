93a37bc56ec8fe3a818e2450d49f1b73cdb185dc
Antgonz16 <antgon@alu.iesserpis.org>
2019-10-28 12:31:31 +0100

    JVectores

---
+++ HolaWorld/src/ted/Jvectores.java
@@ -0,0 +1,20 @@
+package ted;
+
+public class Jvectores {
+
+	public static void main(String[] args) {
+	int index = 0;
+	
+	System.out.println("Vamos a empezar con el programa. ");
+	while (index<=5) {
+	//for (int index=0; index<5; index++)
+		System.out.println("Index");
+		index++;
+	} 
+		System.out.println("Fin del Programa");
+
+		
+		
+	}
+
+}
