67e034d0eaf64f1b3d2f3b37d0052d4e2283386f
Antgonz16 <antgon@alu.iesserpis.org>
2020-02-10 13:54:21 +0100

    Comienzo de la Calculadora

---
+++ CSCalculadora/CSCalculadora/CSCalculadora/MainWindow.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBSieteClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "7");
+    }
+
+    protected void OnBOchoClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "8");
+    }
+
+    protected void OnBNueveClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "9");
+
+    }
+
+    protected void OnBCocienteClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBCuatroClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "4");
+
+    }
+
+    protected void OnBCincoClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "5");
+
+    }
+
+    protected void OnBSeisClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "6");
+
+    }
+
+    protected void OnBProductoClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBUnoClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "1");
+
+    }
+
+    protected void OnBDosClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "2");
+
+    }
+
+    protected void OnBTresClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "3");
+    }
+
+    protected void OnBRestaClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnButton14Clicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBCeroClicked(object sender, EventArgs e)
+    {
+        String display = Pantalla.Text.ToString();
+        Pantalla.DeleteText(0, Pantalla.Text.Length);
+        Pantalla.InsertText(display+ "0");
+    }
+
+    protected void OnButton10Clicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBSumaClicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnBVaciarClicked(object sender, EventArgs e)
+    {
+    }
+}
